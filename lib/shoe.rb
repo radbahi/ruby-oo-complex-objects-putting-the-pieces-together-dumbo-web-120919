e8f622383f38fdfda3f55bc2d8dcba3a1f1fd933
Radouane Bahi <radbahi@gmail.com>
2019-12-10 21:40:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 # Make your shoe class here!
 class Shoe
-attr_accessor :author, :page_count, :genre 
+attr_accessor :size, :material, :condition, :color 
 attr_reader :brand
   
 def initialize(brand)
