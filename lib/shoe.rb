7f82496547ebd96215c5ddc7adbccf3d68461aa8
Radouane Bahi <radbahi@gmail.com>
2019-12-10 21:39:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 # Make your shoe class here!
 class Shoe
 attr_accessor :author, :page_count, :genre 
-attr_reader :title
+attr_reader :brand
   
 def initialize(brand)
 @brand = brand  
 end
 
-  def turn_page
+  def cobble
     puts "Flipping the page...wow, you read fast!"
 end
 
